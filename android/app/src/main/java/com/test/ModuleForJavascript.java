f57660ac205867fb883cf2e50cb582becd370ac0
Rafael Lincoln <rafaellincoln@enesolucoes.com.br>
2017-11-29 11:42:39 -0200

    Log context

--- android/app/src/main/java/com/test/ModuleForJavascript.java
+++ android/app/src/main/java/com/test/ModuleForJavascript.java
@@ -1,16 +1,22 @@
 package com.test;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 public class ModuleForJavascript extends ReactContextBaseJavaModule {
 
     ModuleJavaOnly javaOnly;
+    ReactContext context;
 
     public ModuleForJavascript (ReactApplicationContext reactContext) {
         super(reactContext);
+
+        context = reactContext;
     }
 
     @Override
@@ -21,6 +27,9 @@ public class ModuleForJavascript extends ReactContextBaseJavaModule {
     @ReactMethod
     public void showMyText(Promise promise) {
         javaOnly = new ModuleJavaOnly();
+
+        Log.d("Text:: ", String.format("Context is null: %b", context == null));
+        Log.d("Text:: ", String.format("getApplicationContext is null: %b", context.getApplicationContext() == null));
 
         if (javaOnly.showText() == null) {
             promise.reject("Nenhum texto encontrado");
